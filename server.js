8ec68e527271d18a1ec03c3c2d3f11afd010581c
Tom Siemion <tsiemion07@gmail.com>
2021-06-03 12:26:45 -0500

    updated db connection on local

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const logMachine = require('morgan');
-const routes = ('./routes')
+const logger = require('morgan');
 
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3001;
 
 const application = express();
 
-application.use(logMachine);
+application.use(logger('dev'));
 
 application.use(express.urlencoded({extended: true}));
 application.use(express.json());
 application.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workouttracker', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
 
-application.use(routes);
+application.use(require('./routes/apiRoutes'));
+application.use(require('./routes/uiRoutes'));
 
 application.listen(PORT, () => {
     console.log(`Active Port: ${PORT}`);
